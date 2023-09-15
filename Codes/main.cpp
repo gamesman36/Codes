e41a77e54e13933a9fafe07a1edae2e5de89b992
gamesman36 <gamesman36@outlook.com>
2023-09-15 11:46:04 +0200

    Caesar encryption/decryption

--- Codes/main.cpp
+++ Codes/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void decryptFile(const string& filename);
 void encryptFile(const string& filename);
 int printMenu(string operation);
-string shiftLetters(const string& input, int shiftAmount);
+string shiftLetters(const string& input, int shiftAmount, bool encrypt);
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -36,19 +36,29 @@ int printMenu(string operation) {
             "1. Caesar shift" << endl;
     }
 
+    else if (operation == "decrypt" || operation == "-d") {
+        cout << "Choose cipher to decrypt:" << endl << endl <<
+            "1. Caesar shift" << endl;
+    }
+
     int choice;
     cin >> choice;
     return choice;
 }
 
-string shiftLetters(const string& input, int shiftAmount) {
+string shiftLetters(const string& input, int shiftAmount, bool encrypt) {
     string result = input;
 
     for (char& c : result) {
         if (isalpha(c)) {
             c = tolower(c);
-            c += shiftAmount;
-            c = 'a' + (c - 'a') % 26;
+            if (encrypt) {
+                c += shiftAmount;
+                c = 'a' + (c - 'a') % 26;
+            }
+            else {
+                c = 'z' - ('z' - c + shiftAmount) % 26;
+            }
         }
     }
 
@@ -56,7 +66,35 @@ string shiftLetters(const string& input, int shiftAmount) {
 }
 
 void decryptFile(const string& filename) {
-    // decryption logic goes here
+    int choice = printMenu("decrypt");
+
+    if (choice == 1) {
+        int numberToShift;
+        cout << "How many to shift? ";
+        cin >> numberToShift;
+
+        ifstream inputFile(filename);
+        if (!inputFile) {
+            cerr << "Error opening file." << endl;
+            return;
+        }
+
+        string line;
+        string decryptedContents;
+
+        while (getline(inputFile, line)) {
+            decryptedContents += shiftLetters(line, numberToShift, false) + "\n";
+        }
+
+        ofstream outputFile(filename);
+        if (!outputFile) {
+            cerr << "Couldn't open file.";
+            return;
+        }
+
+        outputFile << decryptedContents;
+        outputFile.close();
+    }
 }
 
 void encryptFile(const string& filename) {
@@ -67,7 +105,6 @@ void encryptFile(const string& filename) {
         cout << "How many to shift? ";
         cin >> numberToShift;
 
-        string contents;
         ifstream inputFile(filename);
         if (!inputFile) {
             cerr << "Error opening file." << endl;
@@ -75,13 +112,21 @@ void encryptFile(const string& filename) {
         }
 
         string line;
+        string encryptedContents;
+
         while (getline(inputFile, line)) {
-            contents += line + "\n";
+            encryptedContents += shiftLetters(line, numberToShift, true) + "\n";
         }
 
         inputFile.close();
 
-        cout << endl << "Contents: " << contents << endl;
-        cout << "Encrypted: " << shiftLetters(contents, numberToShift);
+        ofstream outputFile(filename);
+        if (!outputFile) {
+            cerr << "Couldn't open file.";
+            return;
+        }
+
+        outputFile << encryptedContents;
+        outputFile.close();
     }
 }
